6c3bd537be1184d334eb9d6a5647bc56ab7bb2be
SShine2 <shoshi.a.s@gmail.com>
2017-05-31 22:10:27 -0700

    Add files via upload

---
+++ SoftLaunch/includes/navbar.php
@@ -0,0 +1,18 @@
+<header>
+    <span><h1><img src="images/acelogo.gif" alt="ace in the hole multisport events logo red white black" class="logo"/></h1></span>
+    <div id="nav-icon4">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    </header>
+      
+  <div class="menu" id="menuExpand">
+    <ul>
+        <li <?php if ($currentPage === 'Ace in the Hole Multisport Events | Home') {echo 'class="active"';} ?>><a href="contact.php">Contact</a></li>
+      <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="faqs.php">FAQs</a></li>
+        <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="prepare.php">Prepare</a></li>
+        <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="course.php">Details</a></li>
+        <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="index.php">Home</a></li>
+    </ul>
+  </div>
