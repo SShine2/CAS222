d25801e37b496ad92a75aa40efde60407afab363
SShine2 <shoshi.a.s@gmail.com>
2017-06-13 20:36:55 -0700

    Add files via upload

---
+++ FINAL/includes/footer.php
@@ -0,0 +1,13 @@
+</main>
+
+
+    <footer>
+        <p class="footer"><a href="http://www.twitter.com/pcccas222"><img src="images/tweetfooter.png" alt="twitter logo icon social media" class="pics"></a>    <a href="http://www.facebook.com/cas222cascade"><img src="images/facefooter.png" alt="facebook logo icon social media" class="pics"></a><br><br>
+        Ace in the Hole Multisport Events<br>
+    Copyright &copy; Shoshi Small 2017</p>
+
+    </footer>
+
+  <script type="text/javascript" src="scripts/scripts2.js"></script>
+</body>
+</html>
---
+++ FINAL/includes/navbar.php
@@ -0,0 +1,19 @@
+<header>
+    <span><h1><a href="index.php"><img src="images/acelogo.gif" alt="ace in the hole multisport events logo red white black" class="logo"/></a></h1></span>
+    <div id="nav-icon4">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+   </header>
+    
+  <div class="menu" id="menuExpand">
+    <ul>
+        <li><a href="contact.php">Contact</a></li>
+      <li><a href="faqs.php">FAQs</a></li>
+        <li><a href="prepare.php">Prepare</a></li>
+        <li><a href="course.php">Details</a></li>
+        <li><a href="index.php">Home</a></li>
+    </ul>
+  </div>
+
